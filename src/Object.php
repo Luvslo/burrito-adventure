affc2e4a8b4997fa8820050cc19c1618ceb57915
Jon Nardine and Mary Warrington <marywarrington@gmail.com>
2016-03-07 13:44:57 -0800

    add save and getAll functions, including adjustPunctuation method

--- src/Object.php
+++ src/Object.php
@@ -5,15 +5,15 @@
             private $name;
             private $location;
             private $description;
-            private $id;
             private $game_id;
+            private $id;
 
-            function __construct($name, $location, $description, $id, $game_id) {
+            function __construct($name, $location, $description, $game_id, $id = null) {
                 $this->name = $name;
                 $this->location = $location;
                 $this->description = $description;
-                $this->id = $id;
                 $this->game_id = $game_id;
+                $this->id = $id;
             }
 
             function getName()
@@ -61,7 +61,41 @@
                 $this->game_id = $new_game_id;
             }
 
+            function adjustPunctuation($input)
+            {
+                $search = "/(\')/";
+                $replace = "\'";
+                $clean_input = preg_replace($search, $replace, $input);
+                return $clean_input;
+            }
 
+            function save()
+            {
+                $GLOBALS['DB']->exec("INSERT INTO objects (name, location, description, game_id) VALUES ('{$this->adjustPunctuation($this->getName())}', '{$this->adjustPunctuation($this->getLocation())}', '{$this->adjustPunctuation($this->getDescription())}', {$this->getGameId()});");
+                $this->id = $GLOBALS['DB']->lastInsertId();
+            }
+
+            static function getAll()
+            {
+                $returned_objects = $GLOBALS['DB']->query("SELECT * FROM objects;");
+                $objects = array();
+
+                foreach($returned_objects as $object) {
+                    $name = $object['name'];
+                    $location = $object['location'];
+                    $description = $object['description'];
+                    $game_id = $object['game_id'];
+                    $id = $object['id'];
+                    $new_object = new Object($name, $location, $description, $game_id, $id);
+                    array_push($objects, $new_object);
+                }
+                return $objects;
+            }
+
+            static function deleteAll()
+            {
+                $GLOBALS['DB']->exec("DELETE FROM objects;");
+            }
 
 
 
--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -14,10 +14,10 @@
 
     class ObjectTest extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //     Object::deleteAll();
-        // }
+        protected function tearDown()
+        {
+            Object::deleteAll();
+        }
 
         function test_getInfo()
         {
@@ -28,7 +28,7 @@
             $id = null;
             $game_id = 1;
 
-            $new_object = new Object($name, $location, $description, $id, $game_id);
+            $test_object = new Object($name, $location, $description, $game_id, $id);
             // Act
             $result1 = $name;
             $result2 = $location;
@@ -41,8 +41,46 @@
             $this->assertEquals($description, $result3);
             $this->assertEquals($id, $result4);
             $this->assertEquals($game_id, $result5);
-
-
         }
+
+        function test_Save()
+        {
+            $name = "Frozen Burrito";
+            $location = "Freezer";
+            $description = "This frozen burrito looks super questionable. It's probably been frozen for a long time, and is as hard as a rock. Is that mold I see?";
+            $id = null;
+            $game_id = 1;
+            $test_object = new Object($name, $location, $description, $game_id, $id);
+            $test_object->save();
+
+            $result = Object::getAll();
+
+            $this->assertEquals($test_object, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = "Frozen Burrito";
+            $location = "Freezer";
+            $description = "This frozen burrito looks super questionable. It's probably been frozen for a long time, and is as hard as a rock. Is that mold I see?";
+            $id = null;
+            $game_id = 1;
+
+            $test_object = new Object($name, $location, $description, $game_id, $id);
+            $test_object->save();
+
+            $name2 = "Cash money";
+            $location2 = "Under couch";
+            $description2 = "This is money. It can be used in exchange for goods and services.";
+            $game_id2 = 2;
+
+            $test_object2 = new Object($name2, $location2, $description2, $game_id2, $id);
+            $test_object2->save();
+
+            $result = Object::getAll();
+
+            $this->assertEquals([$test_object, $test_object2], $result);
+        }
+
     }
  ?>
