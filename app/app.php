a89fe767e80e6a542c2ce055ca48e55b842078c6
Jon Nardine and Mary Warrington <jwnardine@gmail.com>
2016-03-07 09:17:40 -0800

    Add initial project files and folders

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Object.php";
+    require_once __DIR__."/../src/Player.php";
+    require_once __DIR__."/../src/Room.php";
+
+    $app = new Silex\Application();
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $server = 'mysql:host=localhost;dbname=burritos';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
---
+++ tests/PlayerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once __DIR__ . '/../src/Player.php';
+
+    $server = 'mysql:host=localhost;dbname=burritos-test';
+    $usercourse_name = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $usercourse_name, $password);
+
+    class PlayerTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Player::deleteAll();
+        }
+
+        function test_methodToTest_inputDescription()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+ ?>
