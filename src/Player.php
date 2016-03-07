9962435c8546bae6703c531cba7bdc0a43993f4d
Marika Allely and Mollie Bootsma <mballely@gmail.com>
2016-03-07 10:52:02 -0800

    add properties to Player class

--- src/Player.php
+++ src/Player.php
@@ -1,8 +1,79 @@
 <?php
 
     class Player
+    {
+        private $name;
+        private $money;
+        private $inventory;
+        private $current_stage;
+        private $has_burrito;
+        private $id;
+
+        function __construct($name, $money = 0, $inventory, $current_stage, $has_burrito = 0, $id = null)
         {
-
-
+            $this->name = $name;
+            $this->money = $money;
+            $this->inventory = $inventory;
+            $this->current_stage = $current_stage;
+            $this->has_burrito = $has_burrito;
+            $this->id = $id;
         }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getMoney()
+        {
+            return $this->money;
+        }
+
+        function getInventory()
+        {
+            return $this->inventory;
+        }
+
+        function getCurrentStage()
+        {
+            return $this->current_stage;
+        }
+
+        function getHasBurrito()
+        {
+            return $this->has_burrito;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        function setMoney($money)
+        {
+            $this->money = $money;
+        }
+
+        function setInventory($inventory)
+        {
+            $this->inventory = $inventory;
+        }
+
+        function setCurrentStage($current_stage)
+        {
+            $this->current_stage = $current_stage;
+        }
+
+        function setHasBurrito($has_burrito)
+        {
+            $this->has_burrito = $has_burrito;
+        }
+
+
+    }
 ?>
--- tests/PlayerTest.php
+++ tests/PlayerTest.php
@@ -7,25 +7,44 @@
 
     require_once __DIR__ . '/../src/Player.php';
 
-    $server = 'mysql:host=localhost;dbname=burritos-test';
+    $server = 'mysql:host=localhost;dbname=burritos_test';
     $usercourse_name = 'root';
     $password = 'root';
     $DB = new PDO($server, $usercourse_name, $password);
 
     class PlayerTest extends PHPUnit_Framework_TestCase
     {
-        protected function tearDown()
-        {
-            Player::deleteAll();
-        }
+        // protected function tearDown()
+        // {
+        //     Player::deleteAll();
+        // }
 
-        function test_methodToTest_inputDescription()
+        function test_getInfo()
         {
             // Arrange
+            $name = "Joe";
+            $money = 0;
+            $inventory = ['frozen_hunk'];
+            $current_stage = 12;
+            $has_burrito = 0;
+            $id = 1;
+            $test_player = new Player($name, $money, $inventory, $current_stage, $has_burrito, $id);
 
             // Act
+            $result1 = $test_player->getName();
+            $result2 = $test_player->getMoney();
+            $result3 = $test_player->getInventory();
+            $result4 = $test_player->getCurrentStage();
+            $result5 = $test_player->getHasBurrito();
+            $result6 = $test_player->getId();
 
             // Assert
+            $this->assertEquals($name, $result1);
+            $this->assertEquals($money, $result2);
+            $this->assertEquals($inventory, $result3);
+            $this->assertEquals($current_stage, $result4);
+            $this->assertEquals(0, $result5);
+            $this->assertEquals($id, $result6);
         }
     }
  ?>
