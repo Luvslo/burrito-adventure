70e34ee25a8de45910610af401d36a0d52eda255
Jon Nardine and Mary Warrington <jwnardine@gmail.com>
2016-03-07 16:33:39 -0800

    Add Stage.php basic methods and tests

--- src/Stage.php
+++ src/Stage.php
@@ -2,10 +2,87 @@
 
     class Stage
         {
-            private $
+            private $name;
             private $description;
+            private $game_id;
             private $id;
 
+            function __construct($name, $description, $game_id, $id = null) {
+                $this->name = $name;
+                $this->description = $description;
+                $this->game_id = $game_id;
+                $this->id = $id;
+            }
+
+            function getName()
+            {
+                return $this->name;
+            }
+
+            function setName($new_name)
+            {
+                $this->name = $new_name;
+            }
+
+            function getDescription()
+            {
+                return $this->description;
+            }
+
+            function setDescription($new_description)
+            {
+                $this->description = $new_description;
+            }
+
+            function getGameId()
+            {
+                return $this->game_id;
+            }
+
+            function setGameId($new_game_id)
+            {
+                $this->game_id = $new_game_id;
+            }
+
+            function getId()
+            {
+                return $this->name;
+            }
+
+            function adjustPunctuation($input)
+            {
+                $search = "/(\')/";
+                $replace = "\'";
+                $clean_input = preg_replace($search, $replace, $input);
+                return $clean_input;
+            }
+
+            function save()
+            {
+                $GLOBALS['DB']->exec("INSERT INTO stages (name, description, game_id) VALUES ('{$this->adjustPunctuation($this->getName())}', '{$this->adjustPunctuation($this->getDescription())}', {$this->getGameId()});");
+                $this->id = $GLOBALS['DB']->lastInsertId();
+            }
+
+            static function getAll()
+            {
+                $returned_stages = $GLOBALS['DB']->query("SELECT * FROM stages;");
+                $stages = array();
+
+                foreach($returned_stages as $stage) {
+                    $name = $stage['name'];
+                    $description = $stage['description'];
+                    $game_id = $stage['game_id'];
+                    $id = $stage['id'];
+                    $new_stage = new Stage($name, $description, $game_id, $id);
+                    array_push($stages, $new_stage);
+                }
+                return $stages;
+            }
+
+            static function deleteAll()
+            {
+                $GLOBALS['DB']->exec("DELETE FROM stages;");
+            }
 
         }
 ?>
--- tests/StageTest.php
+++ tests/StageTest.php
@@ -5,27 +5,55 @@
     * @backupStaticAttributes disabled
     */
 
-    // require_once __DIR__ . '/../src/Stage.php';
-    //
-    // $server = 'mysql:host=localhost;dbname=burritos_test';
-    // $usercourse_name = 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $usercourse_name, $password);
-    //
-    // class StageTest extends PHPUnit_Framework_TestCase
-    // {
-    //     protected function tearDown()
-    //     {
-    //         Stage::deleteAll();
-    //     }
-    //
-    //     function test_methodToTest_inputDescription()
-    //     {
-            // Arrange
+    require_once __DIR__ . '/../src/Stage.php';
 
-            // Act
+    $server = 'mysql:host=localhost;dbname=burritos_test';
+    $usercourse_name = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $usercourse_name, $password);
 
-            // Assert
-    //     }
-    // }
+    class StageTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Stage::deleteAll();
+        }
+
+        function test_Save()
+        {
+            $name = "Kitchen";
+            $description = "This is a kitchen and there's stuff in it";
+            $id = null;
+            $game_id = 1;
+
+            $test_stage = new Stage($name, $description, $game_id, $id);
+            $test_stage->save();
+
+            $result = Stage::getAll();
+
+            $this->assertEquals($test_stage, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = "Kitchen";
+            $description = "This is a kitchen and there's stuff in it";
+            $id = null;
+            $game_id = 1;
+
+            $test_stage = new Stage($name, $description, $game_id, $id);
+            $test_stage->save();
+
+            $name2 = "Bedroom";
+            $description2 = "You wake up in a bedroom. You are hungry for burritos.";
+            $game_id2 = 2;
+
+            $test_stage2 = new Stage($name2, $description2, $game_id2, $id);
+            $test_stage2->save();
+
+            $result = Stage::getAll();
+
+            $this->assertEquals([$test_stage, $test_stage2], $result);
+        }
+    }
  ?>
